2979c78bfa1bd9abb4e165522967af341434774a
diogo <diogo.lima.flooxai@gmail.com>
2025-04-16 07:23:36 -0300

    feat: add useContext

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,20 +1,28 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Menu from '../components/layout/Menu'
-import Content from '../components/layout/Content'
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
 
-const App = props => {
+import { useState } from "react";
+import { DataContext, data } from "../data/DataContext";
+import Store from "../data/Store";
 
-    return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
-    )
-}
+const App = (props) => {
+  const [state, setState] = useState(data);
+  return (
+    <Store>
+      <DataContext.Provider value={{ state, setState }}>
+        <div className="App">
+          <Router>
+            <Menu />
+            <Content />
+          </Router>
+        </div>
+      </DataContext.Provider>
+    </Store>
+  );
+};
 
-export default App
+export default App;
